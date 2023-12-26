e4487c4db4c41475f7682195edfa353811f85f8c
ppsahu <pratyushpsahu2@gmail.com>
2023-12-26 01:13:16 -0800

    problem 2 - reverse an array

---
+++ reverseArray.java
@@ -0,0 +1,24 @@
+public class reverseArray {
+    public static int[] reverse(int[] a){
+        int start = 0;
+        int end = a.length - 1;
+        while (start <= end) {
+            swap(a, start, end);
+            start++;
+            end--;
+        }
+        return a;
+    }
+    public static void swap(int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] inputArray = {0, 1, 2, 3, 4};
+        int[] resultArray = reverse(inputArray);
+        for(int i = 0 ; i < resultArray.length ; i++){
+            System.out.print(resultArray[i] + " ");
+        }
+    }
+}
