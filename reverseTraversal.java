c0a658c2645c5835b1f99c7ec3a8edf0f30ea4c6
ppsahu <pratyushpsahu2@gmail.com>
2023-12-25 19:30:53 -0800

    optimized code in cloneEvenInteger() funciton

--- reverseTraversal.java
+++ reverseTraversal.java
@@ -4,15 +4,9 @@ public class reverseTraversal{
         int i = findLastNumber(a);
         while(i >= 0){
             if(a[i] % 2 == 0){
-                end--;
-                a[end] = a[i];
-                end--;
-                a[end] = a[i];
+                a[--end] = a[i];
             }
-            else{
-                end--;
-                a[end] = a[i];
-            }
+            a[--end] = a[i];
             i--;
         }
         return a;
@@ -26,8 +20,6 @@ public class reverseTraversal{
     }
     public static void main(String[] args) {
         int[] inputArray = {2, 4, 1, 0, 3, -1, -1, -1};
-
-        // Clone even integers
         int[] resultArray = cloneEvenInteger(inputArray);
         for(int i = 0 ; i < resultArray.length ; i++){
             System.out.print(resultArray[i] + " ");
