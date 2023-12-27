7e89ee1c11faa45140d081636afa6a4426c7311c
ppsahu <pratyushpsahu2@gmail.com>
2023-12-26 19:17:02 -0800

    problem 9-kadanes algorithm-find subaaray ofmaxsum

---
+++ kadanesAlgorithm.java
@@ -0,0 +1,31 @@
+// Given an array of integers that can be both +ve and -ve, 
+// find the contiguous subarraywith the largest sum.
+// For example:  [1,2,-1,2,-3,2,-5]  -> first 4 elements have the largest sum. 
+// Return (0,3)
+
+public class kadanesAlgorithm {
+    public static int maxSumSubarray(int[] a){
+        if (a.length == 0) {
+            return 0;
+        }
+        if(a.length == 1){
+            return a[0];
+        }
+        int totalMax = a[0];
+        int runningMax = a[0];
+        for(int ind = 1; ind < a.length ; ind++){
+
+            // kadanes formula
+            // remember always
+            runningMax = Math.max(runningMax + a[ind], a[ind]);
+            totalMax = Math.max(totalMax, runningMax);
+        }
+        return totalMax;
+    }
+    public static void main(String[] args) {
+        int[] inputArray = {-2, -3, -4, -1, 0, -7};
+        int result = maxSumSubarray(inputArray);
+        System.out.println(result);
+        
+    }
+}
