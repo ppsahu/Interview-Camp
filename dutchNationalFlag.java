1291d45393642502609669ecd0a6b9626fcded4b
ppsahu <pratyushpsahu2@gmail.com>
2023-12-26 18:16:47 -0800

    problem 8 - dutch national flag - 0,1,2 in order

--- dutchNationalFlag.java
+++ dutchNationalFlag.java
@@ -1,3 +1,8 @@
+// You're given a list of Marbles. 
+// Each marble can have one of 3 colors (Red, White or Blue).
+// Arrange the marbles in order of the colors (Red -> White -> Blue).
+// Colors are represented as follows:0 - Red1 - White2 - Blue
+
 public class dutchNationalFlag {
     public static int[] dutchFlag(int[] a){
         if(a.length <= 1){
